7bea59446de9ca87ec534f779d53bafed4e2fecd
araki <araki@qoncept.co.jp>
2017-04-12 18:13:02 +0900

    Remove `DataType`

--- Sources/DataType.swift
+++ Sources/DataType.swift
@@ -1,25 +1,4 @@
 
-public protocol DataType {
-    
-}
-
-extension Bool: DataType {}
-
-extension UInt: DataType {}
-extension UInt8: DataType {}
-extension UInt16: DataType {}
-extension UInt32: DataType {}
-extension UInt64: DataType {}
-
-extension Int: DataType {}
-extension Int8: DataType {}
-extension Int16: DataType {}
-extension Int32: DataType {}
-extension Int64: DataType {}
-
-extension Float: DataType {}
-extension Double: DataType {}
-
 public enum NumpyDataType: String {
     case bool = "b1"
     
